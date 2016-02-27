bfbdaa2599adce62dd2940e26ddd4948ff314540
Adam Smith <avianmigration@gmail.com>
2016-02-27 15:16:00 -0500

    Added test script to check download ability

---
+++ test_script.R
@@ -0,0 +1,6 @@
+# These packages should all be installed on your machine
+library(rgdal)        # R bindings for the Geospatial Data Abstraction Library    
+library(raster)       # Primary package for working with rasters in R
+# library(sp)         # For vector data/shapefiles; loaded with raster package
+library(rasterVis)    # Some handy visualization functions for raster data
+library(dplyr)        # Handy tools for data manipulation
